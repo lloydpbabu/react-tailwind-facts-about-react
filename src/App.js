306da6d792b0f6cd5b094dee1859480363c3c10f
Lloyd P Babu <lloydpbabu@gmail.com>
2022-01-25 21:35:10 +0530

    tailwind prettier extension added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-slate-800 h-full w-full flex-col">
-      <div >
-        <nav className="bg-slate-900 p-6 flex flex-col items-center md:flex-row md:items-center w-full md:gap-5 drop-shadow-2xl">
-          <img className="w-28 animate-slowspin" src="logo192.png" alt="logo" />
-          <h1 className="text-cyan-400 text-3xl text-left font-customInterBold">
+    <div className="h-full w-full flex-col bg-slate-800">
+      <div>
+        <nav className="flex w-full flex-col items-center bg-slate-900 p-6 drop-shadow-2xl md:flex-row md:items-center md:gap-5">
+          <img className="animate-slowspin w-28" src="logo192.png" alt="logo" />
+          <h1 className="font-customInterBold text-left text-3xl text-cyan-400">
             ReactFacts
           </h1>
           <div className="md:grow">
-            <h1 className="text-white text-3xl md:text-right">
+            <h1 className="text-3xl text-white md:text-right">
               React Course - Project 1
             </h1>
           </div>
         </nav>
-        <div className="p-6 overflow-y-auto">
-          <h1 className="text-white text-4xl font-customInterBold">
+        <div className="overflow-y-auto p-6">
+          <h1 className="font-customInterBold text-4xl text-white">
             Fun Facts About React
           </h1>
           <ul className="list-disc p-6 text-xl text-white">
@@ -43,7 +43,7 @@ function App() {
           </ul>
         </div>
       </div>
-      <footer className="text-white bg-slate-900 text-center relative bottom-0 w-full p-2">
+      <footer className="relative bottom-0 w-full bg-slate-900 p-2 text-center text-white">
         Made with &#10084; Lloyd P Babu
       </footer>
     </div>
